21cae8dc6d63973ff93c7854d2d0fd263dff76f9
huyamin <772696843@qq.com>
2016-04-15 10:34:56 +0800

    让跳动的动画更加流畅

--- app/src/main/java/zrzn/com/damping_view/fruitview/CurveHeadLoadingView.java
+++ app/src/main/java/zrzn/com/damping_view/fruitview/CurveHeadLoadingView.java
@@ -5,13 +5,13 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
-import android.graphics.RectF;
 import android.os.Handler;
 import android.os.Message;
 import android.support.annotation.StringRes;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
+
 
 /**
  * Created by 胡亚敏 on 2016/3/25.
@@ -22,22 +22,21 @@ public class CurveHeadLoadingView extends View {
     private int mTextColor;//画出文字的具体颜色
     private Paint mTextPaint;//文字画笔
     private final int MIN_HEIGHT = 200;//最小的高度
-    private final int PAINT_TEXT_BASEIINE = 50;//BASELINE的高度
+    private int PAINT_TEXTSIZE = 40;
+    private final int PAINT_TEXT_BASEIINE = PAINT_TEXTSIZE;//BASELINE的高度
     private String mResText;//画出来的文字
     private int mTextWidth;//文字的宽度
-    private int PAINT_TEXTSIZE = 50;
-    private final String DEFAULT_TEXT = "上蔬永辉超市";
-    private final int DEFAULT_RECF_SPACE = 2;//默认的画弧形的时候的间距,值越大速度越快，不能超过最大值
-    private final int MAX_RECF_SPACE = 30;//最大的画弧形的时候的间距
+    private  String mDefaultText ;
+    private final int DEFAULT_RECF_SPACE = 6;//默认的画弧形的时候的间距,值越大速度越快，不能超过最大值
+    private final int MAX_RECF_SPACE = 36;//最大的画弧形的时候的间距
     private final int MIN_RECF_SPACE = -12;//最大的画弧形的时候的间距
     private int mRecfSpace = 0;//矩形RECF间距
     private final int STATUS_DOWN_CURVE = 0;//向下弯曲的状态
     private final int STATUS_UP_CURVE = 1;//向上恢复的状态
     private final int STATUS_FLAT_CURVE = 2;//平的状态
     private int mCurveStatus = STATUS_FLAT_CURVE;
-    private final int MAX_SPRING_COUNT = 25;//来回弹动的时间
+    private final int MAX_SPRING_COUNT = 18;//来回弹动的时间
     private int mSringCount = MAX_SPRING_COUNT;//当前弹动的次数
-    private boolean mStart;
 
     public CurveHeadLoadingView(Context context) {
         super(context);
@@ -58,9 +57,10 @@ public class CurveHeadLoadingView extends View {
     }
 
     void initPaint() {
-        mResText = DEFAULT_TEXT;
+        mDefaultText = "新鲜到家每一天";
+        mResText = mDefaultText;
         mTextPaint = new Paint();
-        mTextPaint.setColor(Color.YELLOW);
+        mTextPaint.setColor(Color.parseColor("#666666"));
         mTextPaint.setTextSize(PAINT_TEXTSIZE);
         mTextPaint.setStyle(Paint.Style.STROKE);
         mTextPaint.setTextAlign(Paint.Align.LEFT);
@@ -124,7 +124,6 @@ public class CurveHeadLoadingView extends View {
             mSringCount++;
             invalidate();
         }else reset(canvas);
-        canvas.drawPath(mPath,mTextPaint);
         canvas.drawTextOnPath(mResText, mPath, 0, 0, mTextPaint);
     }
 
@@ -174,14 +173,13 @@ public class CurveHeadLoadingView extends View {
      */
     void drawArcPath() {
         mPath.reset();
+//        RectF rectF = new RectF(0, PAINT_TEXT_BASEIINE, mTextWidth, PAINT_TEXT_BASEIINE + mRecfSpace);
+//        mPath.addOval(rectF, Path.Direction.CCW);
         mPath.moveTo(0, PAINT_TEXT_BASEIINE); //设定起始点
 //        mPath.lineTo(mTextWidth/5,PAINT_TEXT_BASEIINE);
         mPath.quadTo(0,PAINT_TEXT_BASEIINE,5,PAINT_TEXT_BASEIINE);
         mPath.quadTo(mTextWidth/2,PAINT_TEXT_BASEIINE + mRecfSpace,mTextWidth-5,PAINT_TEXT_BASEIINE);
         mPath.quadTo(mTextWidth*5/6,PAINT_TEXT_BASEIINE,mTextWidth,PAINT_TEXT_BASEIINE);
-//        mPath.lineTo(mTextWidth,PAINT_TEXT_BASEIINE);
-//        RectF rectF = new RectF(0, PAINT_TEXT_BASEIINE, mTextWidth, PAINT_TEXT_BASEIINE + mRecfSpace);
-//        mPath.addOval(rectF, Path.Direction.CCW);
         mPath.close();
     }
 
